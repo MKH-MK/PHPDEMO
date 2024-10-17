8d57f8c6e4edaee2b038059727c78c0bca398082
MKH-MK <not4mak@gmail.com>
2024-10-17 12:17:36 +0100

    Synincing work

---
+++ PHPADDER.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Q2</title>
+    </head>
+    <body>
+        
+		<h1>Add 2 numbers</h1>
+        
+		<form method="POST" action="PHPCALCULATIONQ2.php">
+            <label for="num1">Enter First Number:</label>
+            <input type="number" id="num1" name="num1" required><br>
+			<br>
+            <label for="num2">Enter Second Number:</label>
+            <input type="number" id="num2" name="num2" required><br>
+			<br>
+            <input type="submit" value="Complete">
+        
+		</form>
+    </body>
+</html>
---
+++ PHPCALCULATIONQ2.php
@@ -0,0 +1,9 @@
+ <?php
+ 
+ print_r($_POST);
+	$num1 = $_POST['num1'];
+	$num2 = $_POST['num2'];
+	$sum = $num1 + $num2;
+	echo "<p>The sum is:$sum</p>";
+            
+?>
